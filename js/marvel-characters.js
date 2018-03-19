b3a12fd765c064295e11fdbbe8e59e02f57259c0
Sandhya Rao <smudra@yahoo.com>
2018-03-19 09:25:45 -0500

    created api-request db-interaction fb-config fb-key marvel-characters

---
+++ js/marvel-characters.js
@@ -0,0 +1,11 @@
+"use strict";
+let $ = require('jquery')
+    apiKey = require('./api-request');
+
+const marvelApiURL = apiKey.marvelApiURL;
+
+function getCharactersById(id) {
+    let characterIdUrl = `https://gateway.marvel.com/v1/public/characters`;
+}
+
+/// Not sure this file is necessary. It's not complete.
