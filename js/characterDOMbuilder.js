eca11e3758afd5a96bb4c8a8ef48f05eb49b8b27
Sandhya Rao <smudra@yahoo.com>
2018-03-22 00:15:08 -0500

    Removed info from home and added characterDOMbuilder

--- js/characterDOMbuilder.js
+++ js/characterDOMbuilder.js
@@ -20,12 +20,62 @@ var charInfo;
 //     });
 // }
 
+
+/////----- Creating Index.HTML -----/////
+function displayCharsDom(char) {
+    let showCharsDOM = `<h2>Save Your Favorite Super Hero</h2>
+        <div class="card-deck">
+            <div class="card col-4">
+                <img class="card-img-top" src="images/spiderman.png" alt="Card image cap">
+                <div class="card-body"></div>
+                <div>
+                    <h5 class="card-title">Spider-Man</h5>
+                    <p class="card-text">Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider.</p>
+                </div>
+                <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    <a href="#" class="btn btn-primary float-right disabled">Save</a>
+                </div>
+            </div>
+            <div class="card col-4">
+                <img class="card-img-top" src="images/thor.png" alt="Card image cap">
+                <div class="card-body"></div>
+                <div>
+                <h5 class="card-title">Thor</h5>
+                <p class="card-text">As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                <a href="my-favorites.html" class="btn btn-primary float-right">Save</a>
+                </div>
+            </div>
+            <div class="card col-4">
+                <img class="card-img-top" src="images/black-widow.png" alt="Card image cap">
+                <div class="card-body"></div>
+                <div>
+                    <h5 class="card-title">Black Natasha Widow</h5>
+                    <p class="card-text">Natasha Romanova, known by many aliases, is an expert spy, athlete, and assassin.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                <a href="#" class="btn btn-primary float-right disabled">Save</a>
+                </div>
+            </div>
+        </div>`;
+
+    $('#card-container').html(showCharsDOM);
+
+}
+
 function showChars() {
-    db.getCharactersFB().then((charData) => {
+    db.getCharactersFB()
+    .then((charData) => {
         charInfo = charData;
         console.log("What's in charData ", charData);
     });
 }
 
 
-module.exports = showChars;
+module.exports = {
+    displayCharsDom,
+    showChars};
