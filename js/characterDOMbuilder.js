20219a88e9c01d08b751855543288cfe094f35d3
Sandhya Rao <smudra@yahoo.com>
2018-03-21 23:42:32 -0500

    Added showChars function.

--- js/characterDOMbuilder.js
+++ js/characterDOMbuilder.js
@@ -1,3 +1,31 @@
 "use strict";
+console.log("characterDOMbuilder is in the house");
 
-let $ = require('jquery');
+let $ = require('jquery'),
+    db = require('./db-interaction');
+
+// All variable
+var charInfo;
+
+/////----- Posting Firebase data to DOM -----/////
+
+// --Delete this function. It is for reference only-- //
+// function getCharacterFB(charsObj) {
+//     return $.ajax({
+//         url: `${firebase.getFBsettings().databaseURL}/chars.json?orderBy="uid"`
+//     }).done((resolve) => {
+//         return resolve;
+//     }).fail((error) => {
+//         return error;
+//     });
+// }
+
+function showChars() {
+    db.getCharactersFB().then((charData) => {
+        charInfo = charData;
+        console.log("What's in charData ", charData);
+    });
+}
+
+
+module.exports = showChars;
