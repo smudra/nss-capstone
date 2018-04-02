0c77fc6731c41b6c26f8822cec182dc567c34e5e
Sandhya Rao <smudra@yahoo.com>
2018-04-02 01:13:27 -0500

    Cleaned up commented area

--- js/buildFBObj.js
+++ js/buildFBObj.js
@@ -17,37 +17,6 @@ function buildUserObj(uid) {
     };
     return userObj;
 }
-
-
-// get character index from function to populate in charObj
-// Under id add character index which is character > Object.key. 
-// Char info should go under id
-
-// let userChar = firebase.database().ref("userCharacter");
-
-// function buildUserCharObj(characters) {
-//     let userChar = {
-//         id: $("#userCharid3").key,
-//         uid: user.getUser().uid,
-//         charNotes: ""
-//     };
-//     return userChar;
-// }
-
-
-// function buildUserCharObj(characters) {
-//     var userChar = {
-//         id: characterDOMbuilder.listCharacters(),
-//         uid: user.getUser(),
-//         notes: ""
-//     };
-//     console.log("Het hey", Object.values(charObj.id));
-//     return userChar;
-// // }
-// console.log("Outside", Object.values(buildUserCharObj));
-
-
 module.exports = {
-    buildUserObj, 
-    // buildUserCharObj
+    buildUserObj
 };
